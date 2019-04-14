1afb3637a7665e7e4f15dcbd2aa3f027f16553e9
conankzhang <conankzhang@gmail.com>
2019-04-14 15:09:19 -0600

    MyGame: Reorganize scene for shader display

--- MyGame_/MyGame/cMyGame.cpp
+++ MyGame_/MyGame/cMyGame.cpp
@@ -23,9 +23,9 @@ void eae6320::cMyGame::SubmitDataToBeRendered(const float i_elapsedSecondCount_s
 	eae6320::Graphics::SubmitCamera(m_camera->GetWorldToCameraTransform(i_elapsedSecondCount_sinceLastSimulationUpdate), m_camera->GetCameraToProjectedTransform(), i_elapsedSecondCount_systemTime, i_elapsedSecondCount_sinceLastSimulationUpdate);
 
 	eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object1->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object1->GetPosition()), m_object1->GetMesh(), m_object1->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
-	eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object2->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object2->GetPosition()), m_object2->GetMesh(), m_object2->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
-	eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object3->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object3->GetPosition()), m_object3->GetMesh(), m_object3->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
-	eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object4->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object4->GetPosition()), m_object4->GetMesh(), m_object4->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
+	//eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object2->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object2->GetPosition()), m_object2->GetMesh(), m_object2->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
+	//eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object3->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object3->GetPosition()), m_object3->GetMesh(), m_object3->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
+	//eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object4->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object4->GetPosition()), m_object4->GetMesh(), m_object4->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
 	eae6320::Graphics::SubmitDrawCommand(eae6320::Graphics::RenderCommand::Draw, m_object5->GetEffect(), m_camera->CalculateNormalizedCameraDistance(m_object5->GetPosition()), m_object5->GetMesh(), m_object5->GetTransform(i_elapsedSecondCount_sinceLastSimulationUpdate));
 }
 
@@ -107,9 +107,9 @@ void eae6320::cMyGame::UpdateSimulationBasedOnInput()
 void eae6320::cMyGame::UpdateSimulationBasedOnTime(const float i_elapsedSecondCount_sinceLastUpdate)
 {
 	m_object1->Update(i_elapsedSecondCount_sinceLastUpdate);
-	m_object2->Update(i_elapsedSecondCount_sinceLastUpdate);
-	m_object3->Update(i_elapsedSecondCount_sinceLastUpdate);
-	m_object4->Update(i_elapsedSecondCount_sinceLastUpdate);
+	//m_object2->Update(i_elapsedSecondCount_sinceLastUpdate);
+	//m_object3->Update(i_elapsedSecondCount_sinceLastUpdate);
+	//m_object4->Update(i_elapsedSecondCount_sinceLastUpdate);
 	m_object5->Update(i_elapsedSecondCount_sinceLastUpdate);
 	m_camera->Update(i_elapsedSecondCount_sinceLastUpdate);
 }
@@ -121,10 +121,10 @@ eae6320::cResult eae6320::cMyGame::Initialize()
 {
 	auto result = Results::Success;
 
-	m_object1 = new eae6320::cGameObject(eae6320::Math::sVector(-1.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/white.effect");
-	m_object2 = new eae6320::cGameObject(eae6320::Math::sVector(-0.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/red.effect");
-	m_object3 = new eae6320::cGameObject(eae6320::Math::sVector(0.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/white.effect");
-	m_object4 = new eae6320::cGameObject(eae6320::Math::sVector(1.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/red.effect");
+	m_object1 = new eae6320::cGameObject(eae6320::Math::sVector(0.0f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/local.effect");
+	//m_object2 = new eae6320::cGameObject(eae6320::Math::sVector(-0.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/red.effect");
+	//m_object3 = new eae6320::cGameObject(eae6320::Math::sVector(0.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/white.effect");
+	//m_object4 = new eae6320::cGameObject(eae6320::Math::sVector(1.5f, 0.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/sphere.mesh", "data/Effects/red.effect");
 	m_object5 = new eae6320::cGameObject(eae6320::Math::sVector(0.0f, -1.0f, 0.0f), eae6320::Math::cQuaternion(), "data/Meshes/plane.mesh", "data/Effects/white.effect");
 
 
@@ -144,26 +144,26 @@ eae6320::cResult eae6320::cMyGame::CleanUp()
 		m_object1 = nullptr;
 	}
 
-	if (m_object2)
-	{
-		m_object2->CleanUp();
-		delete m_object2;
-		m_object2 = nullptr;
-	}
+	//if (m_object2)
+	//{
+	//	m_object2->CleanUp();
+	//	delete m_object2;
+	//	m_object2 = nullptr;
+	//}
 
-	if (m_object3)
-	{
-		m_object3->CleanUp();
-		delete m_object3;
-		m_object3 = nullptr;
-	}
+	//if (m_object3)
+	//{
+	//	m_object3->CleanUp();
+	//	delete m_object3;
+	//	m_object3 = nullptr;
+	//}
 
-	if (m_object4)
-	{
-		m_object4->CleanUp();
-		delete m_object4;
-		m_object4 = nullptr;
-	}
+	//if (m_object4)
+	//{
+	//	m_object4->CleanUp();
+	//	delete m_object4;
+	//	m_object4 = nullptr;
+	//}
 
 	if (m_object5)
 	{
